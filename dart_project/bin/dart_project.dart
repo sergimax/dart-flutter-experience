241799c6a43b5f8636165e19945e1bcaa5c2b5a6
Sergienko Maxim <creativesun@yandex.ru>
2023-10-31 20:43:14 +0300

    chor() main cleared

--- dart_project/bin/dart_project.dart
+++ dart_project/bin/dart_project.dart
@@ -1,22 +1,5 @@
 import 'package:dart_project/dart_project.dart' as dart_project;
-import 'dart:math';
-import 'package:characters/characters.dart';
 
 void main(List<String> arguments) {
-    print('Hello world: ${dart_project.calculate()}!');
-    print(sin(45 * pi / 180));
-    print(sqrt(2));
-    const int myAge = 100;
-    double averageAge = 100;
-    averageAge = 150;
-    const testNumber = 9;
-    print(testNumber);
-    const evenOdd = testNumber % 2;
-    print(evenOdd);
-    var alpha = 100;
-    alpha += evenOdd;
-    print(alpha);
-
-    const family = "👨‍👩‍👧‍👦";
-    print(family.characters.length);
+    
 }
